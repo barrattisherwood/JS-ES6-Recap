cc175d64d8de4ec8dd477a3e95dfe94c3dad0bbd
BIsherwood <barratti@osiristrading.co.za>
2019-06-05 17:52:19 +0200

    lesson 38

---
+++ arrays/notes.js
@@ -0,0 +1,14 @@
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+
+// console.log('removed: ', notes.pop());
+// notes.push('My new note')
+
+// console.log('shifted', notes.shift());
+// console.log('shifted', notes.unshift('Very first note'));
+
+// console.log('Second item: ', notes.splice(1, 1, 'This is the new second item'))
+
+notes[2] = 'This is the new note 3'
+
+console.log('notes: ', notes.length);
+console.log('notes[last]: ', notes);
---
+++ arrays/todo.js
@@ -0,0 +1,8 @@
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items -> Todo: walk the dog
+
+const todos = ['walk the dog', 'pack firewood', 'drink a beer', 'go to sleep', 'rave hard asf']
+console.log(`Todos: ${todos.length}`);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);
