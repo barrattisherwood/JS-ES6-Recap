8f15409c159c861524189331a77ad13013a20d82
BIsherwood <barratti@osiristrading.co.za>
2019-06-05 17:58:39 +0200

    Lesson 39

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,8 +1,8 @@
-// Create an array with five todos
-// You have x todos
-// Print the first and second to last items -> Todo: walk the dog
-
 const todos = ['walk the dog', 'pack firewood', 'drink a beer', 'go to sleep', 'rave hard asf']
-console.log(`Todos: ${todos.length}`);
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`);
+
+todos.splice(2,1)
+todos.push('last item')
+todos.shift()
+
+console.log(`Number of todos: ${todos.length}`);
+console.log(todos);
