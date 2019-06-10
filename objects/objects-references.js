5f4ae517fdc8a030723cb8d958f05dcb583b7996
BIsherwood <barratti@osiristrading.co.za>
2019-06-10 17:53:20 +0200

    completed Lesson 31

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -3,12 +3,38 @@ let myAccount = {
     expenses: 0,
     income: 0
 }
-let otherAccount = myAccount
-otherAccount.income = 1000;
 
 let addExpense = function(account, amount) {
     account.expenses += amount
 }
 
-addExpense(myAccount, 2.50);
-console.log(myAccount);
+// addIncome
+let addIncome = function(account, income) {
+    account.income += income;
+}
+
+// resetAccount
+let resetAccount = function(account) {
+    account.expenses = 0;
+    account.income = 0;
+}
+
+// getAccountSummary
+// Account for Andrew has $900. $1000 in income. $100 in expenses.
+let getAccountSummary = function(account) {
+    return `Account for ${account.name} has $${account.income - account.expenses}. $${account.income} in income. $${account.expenses} in expenses.`;
+}
+
+// addIncome
+addIncome(myAccount, 200);
+addIncome(myAccount, 1200);
+// addExpense
+addExpense(myAccount, 100);
+// addExpense
+addExpense(myAccount, 150);
+// getAccountSummary
+console.log(getAccountSummary(myAccount));
+// resetAccount
+resetAccount(myAccount);
+// getAccountSummary
+console.log(getAccountSummary(myAccount));
