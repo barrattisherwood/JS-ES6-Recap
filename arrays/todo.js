2551aad3ec4e1ada34d39ea005759be75bf280fe
BIsherwood <barratti@osiristrading.co.za>
2019-06-14 17:09:06 +0200

    Lesson 43

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,22 +1,35 @@
-const todos = ['walk the dog', 'pack firewood', 'drink a beer', 'go to sleep', 'rave hard asf']
+const todos = [
+    {
+        text: 'walk the dog',
+        completed: false
+    },
+    {
+        text: 'pack firewood',
+        completed: false
+    },
+    {
+        text: 'drink a beer',
+        completed: true
+    },
+    {
+        text: 'go to sleep',
+        completed: false
+    },
+    {
+        text: 'rave hard asf',
+        completed: true
+    }]
+    
+console.log('todos: ', todos);
 
-todos.splice(2,1)
-todos.push('last item')
-todos.shift()
-
-console.log(`Number of todos: ${todos.length}`);
-console.log(todos);
-
-// TODO
-
-// 1. The first item
-// 2. The second item
-// 3. The third item
-
-todos.forEach(function(item, index){
-    console.log(`${index + 1}. ${item}`);
-});
-console.log(`----------------------`);
-for (let i = 0; i < todos.length; i++) {
-    console.log(`${i + 1}. ${todos[i]}`);
+let deleteTodo = function (todos, todoTitleToBeDeleted) {
+    const index = todos.findIndex(function(todo) {
+        return todo.text.toLowerCase() === todoTitleToBeDeleted.toLowerCase()
+    });
+    if (index !== -1) {
+        todos.splice(index, 1);
+    }    
 }
+
+deleteTodo(todos, 'go to sleep');
+console.log('todos', todos);
