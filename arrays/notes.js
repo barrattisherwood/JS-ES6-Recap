c8f40ada3ee2e2a920f0c97c113c1137c8180d71
BIsherwood <barratti@osiristrading.co.za>
2019-06-24 18:33:56 +0200

    refactor(sorting): Custom sort function

--- arrays/notes.js
+++ arrays/notes.js
@@ -9,11 +9,25 @@ const notes = [{
     body: 'Get a new seat'
 }];
 
-const findNote = function(notes, noteTitle) {
-    return notes.find(function(note, index){
-        return note.title.toLowerCase() === noteTitle.toLowerCase()
+console.log('a' < 'A');
+
+const sortNotes = function (notes) {
+    notes.sort(function (a, b) {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
     })
 }
+
+// const findNote = function(notes, noteTitle) {
+//     return notes.find(function(note, index){
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+// }
 
 // const findNote = function(notes, noteTitle) {
 //     const index = notes.findIndex(function(note, index){
@@ -30,7 +44,9 @@ const findNotes = function(notes, searchTerm) {
     })
 }
 
-console.log(findNotes(notes, 'work'));
-
+// console.log(findNotes(notes, 'work'));
 // const note = findNote(notes, 'Office modification');
 // console.log('note: ', note);
+
+sortNotes(notes);
+console.log(notes);
